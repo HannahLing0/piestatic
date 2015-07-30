5d66995ea5b524b5840e819318c7e38d27be5bae
Christine Wang <christine.wang.395@gmail.com>
2015-07-29 20:36:51 -0700

    Fixed some bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
 $(function() {
     //Get the HTML from the template in the script tag
     var navbarTemplateSource = $("#navbar-template").html();
+    console.log("script started");
     var indexTemplateSource = $("#index-template").html();
     //Compile the template
-    var navbarTemplate = Handlebars.compile(navbarTemplateSource);
+    // var navbarTemplate = Handlebars.compile(navbarTemplateSource);
     var indexTemplate = Handlebars.compile(indexTemplateSource);
-    // The function will insert all the values from the objects in their respective places in the HTML and returned HTML as a string. Then we use jQuery to append the resulting HTML string into the page
-    $(document.body).append(indexTemplate());
+
+    // var data = {};
+
+    // The function will insert all the values from the objects in their r
+    // espective places in the HTML and returned HTML as a string. Then we use
+    // jQuery to append the resulting HTML string into the page
+    Handlebars.registerPartial("navbar", navbarTemplateSource);
+    $("body").append(indexTemplate({}));
     console.log("script finished");
 });
