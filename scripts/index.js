5723b2bbc7e937d9cab4c950dbe1ec84d2fa5814
Christine Wang <christine.wang.395@gmail.com>
2015-07-30 21:36:18 -0700

    Fixed some font import errors.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 $(function() {
+    console.log("script started");
+
     //Get the HTML from the template in the script tag
     var navbarTemplateSource = $("#navbar-template").html();
-    console.log("script started");
     var indexTemplateSource = $("#index-template").html();
     //Compile the template
     // var navbarTemplate = Handlebars.compile(navbarTemplateSource);
@@ -9,10 +10,10 @@ $(function() {
 
     // var data = {};
 
-    // The function will insert all the values from the objects in their r
-    // espective places in the HTML and returned HTML as a string. Then we use
+    // The function will insert all the values from the objects in their
+    // respective places in the HTML and returned HTML as a string. Then we use
     // jQuery to append the resulting HTML string into the page
     Handlebars.registerPartial("navbar", navbarTemplateSource);
-    $("body").append(indexTemplate({}));
+    $("body").append(indexTemplate());
     console.log("script finished");
 });
