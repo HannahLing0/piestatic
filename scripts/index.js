c60074ca17f05ed4eeaf69cb91a1bd2a5dac6f3a
Christine Wang <christine.wang.395@gmail.com>
2015-07-30 23:35:26 -0700

    Attempted to put navbar in separate HTML file.

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,38 @@
+// function source: http://stackoverflow.com/questions/8366733/external-template-in-underscore
+function render(tmpl_name, tmpl_data) {
+  if (!render.tmpl_cache) {
+    render.tmpl_cache = {};
+  }
+  if (!render.tmpl_cache[tmpl_name]) {
+    var tmpl_dir = '../templates';
+    var tmpl_url = tmpl_dir + '/' + tmpl_name + '.html';
+    var tmpl_string;
+    $.ajax({
+      url: tmpl_url,
+      method: 'GET',
+      async: false,
+      success: function(data) {
+        tmpl_string = data;
+      }
+    });
+    render.tmpl_cache[tmpl_name] = Handlebars.compile(tmpl_string);
+  }
+  return render.tmpl_cache[tmpl_name](tmpl_data);
+}
+
 $(function() {
-    console.log("script started");
+  //Get the HTML from the template in the script tag
+  // var navbarTemplateSource = $('#navbar-template').html();
+  // var indexTemplateSource = $('#index-template').html();
+  //Compile the template
+  // var navbarTemplate = Handlebars.compile(navbarTemplateSource);
+  // var indexTemplate = Handlebars.compile(indexTemplateSource);
+  var navbarTemplate = render('navbar', {});
+  var indexTemplate = render('index', {});
 
-    //Get the HTML from the template in the script tag
-    var navbarTemplateSource = $("#navbar-template").html();
-    var indexTemplateSource = $("#index-template").html();
-    //Compile the template
-    // var navbarTemplate = Handlebars.compile(navbarTemplateSource);
-    var indexTemplate = Handlebars.compile(indexTemplateSource);
-
-    // var data = {};
-
-    // The function will insert all the values from the objects in their
-    // respective places in the HTML and returned HTML as a string. Then we use
-    // jQuery to append the resulting HTML string into the page
-    Handlebars.registerPartial("navbar", navbarTemplateSource);
-    $("body").append(indexTemplate());
-    console.log("script finished");
+  // The function will insert all the values from the objects in their
+  // respective places in the HTML and returned HTML as a string. Then we use
+  // jQuery to append the resulting HTML string into the page
+  Handlebars.registerPartial('navbar', navbarTemplate);
+  $('body').append(indexTemplate());
 });
